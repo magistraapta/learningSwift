ecad46271b6596a7e41937a6a5ca6c511c4e3270
Magistra <magistraagis@gmail.com>
2023-06-17 14:48:37 +0800

    initial commit

--- testingSwift/ContentView.swift
+++ testingSwift/ContentView.swift
@@ -8,16 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text: String = "hello world!"
+    @State var count: Int = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(text)
+                .bold()
+                .font(.title)
+            Text("count: \(count)")
+            Button(action:{
+                buttonPressed()
+            },label: {
+                Text("this is button")
+                    .foregroundColor(.white)
+                    .padding()
+                    .cornerRadius(10)
+                    .background(Color.blue)
+                    .bold()
+            })
+            Button(action:{
+                addNumber()
+            },label: {
+                Text("add the number")
+                    .foregroundColor(.white)
+                    .padding()
+                    .cornerRadius(10)
+                    .background(Color.blue)
+                    .bold()
+            })
+            Button(action:{
+                reset()
+            },label: {
+                Text("reset the number")
+                    .foregroundColor(.white)
+                    .padding()
+                    
+                    .background(Color.blue)
+                    .bold()
+            }).cornerRadius(10)
         }
-        .padding()
     }
+    func buttonPressed(){
+        text = "button has been pressed!"
+    }
+    
+    func addNumber(){
+        count = count + 1
+    }
+    
+    func reset(){
+        count = 0
+    }
+
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
