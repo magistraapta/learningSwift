3ca98f7042d52c2a48b40927dcd16459fa3cfbe3
Magistra <magistraagis@gmail.com>
2023-06-17 14:07:47 +0800

    Initial Commit

---
+++ testingSwift/testingSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingSwiftApp.swift
+//  testingSwift
+//
+//  Created by magistra aptam on 17/06/23.
+//
+
+import SwiftUI
+
+@main
+struct testingSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
